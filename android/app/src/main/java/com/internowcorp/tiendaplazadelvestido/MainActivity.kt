cef6b1b75b4ae19a677ed80d479f5d84acfc963e
Alexander <alexander.va@hotmail.com>
2025-12-29 08:24:11 -0300

    feat: update bundle id

--- android/app/src/main/java/com/internowcorp/tiendaplazadelvestido/MainActivity.kt
+++ android/app/src/main/java/com/internowcorp/tiendaplazadelvestido/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.basewebviewapp
+package com.internowcorp.tiendaplazadelvestido
 
 import android.os.Bundle
 import com.facebook.react.ReactActivity
--- android/app/src/main/java/com/internowcorp/tiendaplazadelvestido/MainApplication.kt
+++ android/app/src/main/java/com/internowcorp/tiendaplazadelvestido/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.basewebviewapp
+package com.internowcorp.tiendaplazadelvestido
 
 import android.app.Application
 import com.facebook.react.PackageList
